f5b2f7783bfd03ca0ea9235ad550ad905b3cf386
acupoframen <choe.hwan@gmail.com>
2024-11-07 16:00:40 +0900

    celery 3분 주석처리

--- backend/moonsunpowerback/moonsunpowerback/celery.py
+++ backend/moonsunpowerback/moonsunpowerback/celery.py
@@ -14,6 +14,8 @@ app.conf.beat_schedule = {
         'task': 'moonsunpowerai.tasks.generate_and_save_text',
         'schedule': crontab(hour=0, minute=0),  # 매일 자정에 실행
         'options': {'timezone': 'Asia/Seoul'},  # 한국 시간 기준
+        # 'schedule': crontab(minute='*/2'),  # Runs every 2 minutes
+        # 'options': {'timezone': 'Asia/Seoul'},  # Korean time zone
     },
 }
 app.conf.worker_pool = 'solo'
