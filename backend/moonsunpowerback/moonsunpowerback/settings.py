bbab94ab3f88c138d5fb68044bdc23610a6a679b
acupoframen <choe.hwan@gmail.com>
2024-11-17 22:35:31 +0900

    django setting에 moonsunpower 포함

--- backend/moonsunpowerback/moonsunpowerback/settings.py
+++ backend/moonsunpowerback/moonsunpowerback/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.38.179.92','127.0.0.1']
+ALLOWED_HOSTS = ['3.38.179.92','127.0.0.1', 'moonsunpower.com']
 
 
 # Application definition
