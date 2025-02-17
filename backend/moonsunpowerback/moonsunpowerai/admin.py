a1aa6d4b19dafbc4f95939451bee3cc5d30470c3
acupoframen <choe.hwan@gmail.com>
2025-02-17 16:57:24 +0900

    admin 설정 추가 및 admin에 text 화면 확인 가능

--- backend/moonsunpowerback/moonsunpowerai/admin.py
+++ backend/moonsunpowerback/moonsunpowerai/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import QuestionItem, GeneratedText, Text
 
-# Register your models here.
+@admin.register(QuestionItem)
+class QuestionItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_text', 'answer')  # 원하는 필드 추가
+
+@admin.register(GeneratedText)
+class GeneratedTextAdmin(admin.ModelAdmin):
+    list_display = ('id', 'subject', 'date')
+
+@admin.register(Text)
+class TextAdmin(admin.ModelAdmin):
+    list_display = ('id', 'subject', 'date')
--- backend/moonsunpowerback/moonsunpowerback/settings.py
+++ backend/moonsunpowerback/moonsunpowerback/settings.py
@@ -156,7 +156,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-STATIC_URL = 'django_static/'
+STATIC_URL = '/django_static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
