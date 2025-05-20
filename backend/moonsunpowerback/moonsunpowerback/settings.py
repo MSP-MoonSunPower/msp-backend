75be75931ce8b36685d924b42990fdd2ab6ed3ca
acupoframen <choe.hwan@gmail.com>
2025-05-20 16:49:51 +0900

    feat: add default permission classes to REST framework settings

--- backend/moonsunpowerback/moonsunpowerback/settings.py
+++ backend/moonsunpowerback/moonsunpowerback/settings.py
@@ -109,8 +109,10 @@ WSGI_APPLICATION = 'moonsunpowerback.wsgi.application'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        ),
-
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 
